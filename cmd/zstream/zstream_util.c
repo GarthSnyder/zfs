f866bee5c0e481c846990053291a5e97eb6b4198
Garth Snyder <garth@garthsnyder.com>
2026-03-16 19:14:39 -0700

    Restore dump_record() to zstream_util.[ch] to enable zstream_drop_record.c to compile

--- cmd/zstream/zstream_util.c
+++ cmd/zstream/zstream_util.c
@@ -39,6 +39,58 @@
 
 #include "zstream_util.h"
 
+/*
+ * Originally from libzfs_sendrecv.c, but not identical. Emit a replay
+ * record with proper checksums and with proper maintenance of the stream
+ * checksum passed in as zc.
+ *
+ * DRR_BEGIN records do not have record checksums. They can't, because the
+ * drr_begin struct overlaps with space that would otherwise be used for the
+ * end-record checksum.
+ *
+ * DRR_END records normally do have end-record checksums. However, records
+ * emitted by send_conclusion_record() in libzfs_sendrecv.c have the
+ * checksum set to zero. zfs receive ignores those.
+ *
+ * Null zstream transformations should be idempotent. E.g., a zstream redup
+ * that does not redup anything should yield a stream that is bit-for-bit
+ * identical to the original stream. So, it's helpful to emulate zfs send's
+ * checksumming practices just to minimize spurious differences between
+ * input and output streams.
+ *
+ * Note that there are two separate checksums in a DRR_END record. One is
+ * the drr->drr_u.drr_end.drr_checksum field, which is a stream-to-date
+ * checksum that is always filled out and always validated. The other
+ * checksum is the end-record drr->drr_u.drr_checksum.drr_checksum field,
+ * which may or may not be present.
+ */
+int
+dump_record(dmu_replay_record_t *drr, void *payload, size_t payload_len,
+    zio_cksum_t *zc, int outfd)
+{
+	ASSERT3U(offsetof(dmu_replay_record_t, drr_u.drr_checksum.drr_checksum),
+	    ==, sizeof (dmu_replay_record_t) - sizeof (zio_cksum_t));
+	fletcher_4_incremental_native(drr,
+	    offsetof(dmu_replay_record_t, drr_u.drr_checksum.drr_checksum), zc);
+	boolean_t is_conclusion_record =
+	    drr->drr_type == DRR_END &&
+	    drr->drr_u.drr_end.drr_toguid == 0 &&
+	    ZIO_CHECKSUM_IS_ZERO(&drr->drr_u.drr_checksum.drr_checksum);
+	if (!is_conclusion_record && drr->drr_type != DRR_BEGIN) {
+		drr->drr_u.drr_checksum.drr_checksum = *zc;
+	}
+	fletcher_4_incremental_native(&drr->drr_u.drr_checksum.drr_checksum,
+	    sizeof (zio_cksum_t), zc);
+	if (write(outfd, drr, sizeof (*drr)) == -1)
+		return (errno);
+	if (payload_len != 0) {
+		fletcher_4_incremental_native(payload, payload_len, zc);
+		if (write(outfd, payload, payload_len) == -1)
+			return (errno);
+	}
+	return (0);
+}
+
 void *
 safe_malloc(size_t size)
 {
