1474ac93d76fa2325b9316083e8171b8ee4c777d
Garth Snyder <garth@garthsnyder.com>
2026-03-18 15:43:27 -0700

    Tune stream generator script

--- cmd/zstream/scripts/make-test-streams.sh
+++ cmd/zstream/scripts/make-test-streams.sh
@@ -12,7 +12,8 @@ zpool create -o ashift=12 test "$DEVICE"
 zfs set compression=on xattr=sa test
 zfs create test/source
 
-"$SCRIPTDIR/gen-lorem-files.py" -d /test/source 3
+"$SCRIPTDIR/gen-lorem-files.py" -d /test/source --min-size 2048 \
+    --max-size 32000 3
 "$SCRIPTDIR/add-xattrs.py" /test/source/*
 echo "very small" > /test/source/small
 echo "password" > /test/source/to-be-redacted
@@ -21,16 +22,24 @@ chmod 400 /test/source/to-be-redacted
 zfs snapshot -r test/source@baseline
 zfs clone test/source@baseline test/redacted
 rm /test/redacted/to-be-redacted
+"$SCRIPTDIR/gen-lorem-files.py" -d /test/redacted --min-size 4096 \
+    --max-size 32000 3
+"$SCRIPTDIR/add-xattrs.py" /test/redacted/*
+cd /test/redacted
+tar cf /tmp/dups.tar .
+mkdir copies
+cd copies
+tar xvf /tmp/dups.tar
 
 echo "password" > /test/redacted/new-key
 zfs create -o encryption=on -o keylocation=file:///test/redacted/new-key \
     -o keyformat=passphrase test/redacted/encrypted
 "$SCRIPTDIR/gen-lorem-files.py" -d /test/redacted/encrypted 3
 echo "very small" > /test/redacted/encrypted/small-encrypted
-"$SCRIPTDIR/add-xattrs.py" /test/redacted/encrypted/*
+# "$SCRIPTDIR/add-xattrs.py" /test/redacted/encrypted/*
 
 zfs snapshot -r test/redacted@clean
 
 zfs redact test/source@baseline redaction-bookmark test/redacted@clean
-zfs send -e --redact redaction-bookmark test/source@baseline > /tmp/part1.zfs
-zfs send -Rew -i test/source@baseline test/redacted@clean > /tmp/part2.zfs
+zfs send -ce --redact redaction-bookmark test/source@baseline > /tmp/all-record-types-base.zsend
+zfs send -Rcew -i test/source@baseline test/redacted@clean > /tmp/all-record-types-incr.zsend
