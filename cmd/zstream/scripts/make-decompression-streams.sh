6af12bfa2610fd1308f823ab85d2063cafad6a6c
Garth Snyder <garth@garthsnyder.com>
2026-03-30 14:05:38 -0700

    Stream generator script adjustments

--- cmd/zstream/scripts/make-decompression-streams.sh
+++ cmd/zstream/scripts/make-decompression-streams.sh
@@ -12,12 +12,14 @@ zpool create -o ashift=12 test "$DEVICE"
 echo "password" > /test/password
 
 zfs create -o compression=zstd-5 test/unencrypted
-"$SCRIPTDIR/gen-lorem-files.py" -d /test/unencrypted --min-size 100000 \
-    --max-size 200000 5
+"$SCRIPTDIR/gen-lorem-files.py" -r -d /test/unencrypted --min-size 12000 \
+    --max-size 40000 2
+"$SCRIPTDIR/gen-lorem-files.py" -r -d /test/unencrypted --min-size 140000 \
+    --max-size 160000 1
 
 zfs create -o compression=lz4 -o encryption=on -o keylocation=file:///test/password -o keyformat=passphrase test/encrypted
-"$SCRIPTDIR/gen-lorem-files.py" -d /test/encrypted --min-size 100000 \
-    --max-size 200000 5
+"$SCRIPTDIR/gen-lorem-files.py" -r -d /test/encrypted --min-size 12000 \
+    --max-size 40000 3
 
 zfs snapshot -r test@decompression
 zfs send -cw test/unencrypted@decompression > /tmp/decompression.zsend
