e999b3e7234000ee0a8ec917b51f301074991be8
Garth Snyder <garth@garthsnyder.com>
2026-05-17 13:55:56 -0700

    rm zfs_refcount_init/fini

--- cmd/zstream/zstream_recompress.c
+++ cmd/zstream/zstream_recompress.c
@@ -99,7 +99,6 @@ zstream_do_recompress(int argc, char *argv[])
 		exit(1);
 	}
 
-	zfs_refcount_init();
 	abd_init();
 	fletcher_4_init();
 	zio_init();
@@ -354,7 +353,6 @@ zstream_do_recompress(int argc, char *argv[])
 	zio_fini();
 	zstd_fini();
 	abd_fini();
-	zfs_refcount_fini();
 
 	return (0);
 }
