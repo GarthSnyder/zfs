909ce2c8d235dd993552880973215fc00348414d
Garth Snyder <garth@garthsnyder.com>
2026-05-17 14:48:14 -0700

    Add CDDL headers for scripts

--- cmd/zstream/scripts/make-dump-files.py
+++ cmd/zstream/scripts/make-dump-files.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 """Run old and new zstream dump -v on stream files, producing abbreviated dump outputs."""
+
+#
+# SPDX-License-Identifier: CDDL-1.0
+#
+# CDDL HEADER START
+#
+# This file and its contents are supplied under the terms of the Common
+# Development and Distribution License ("CDDL"), version 1.0. You may only use
+# this file in accordance with the terms of version 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this source. A
+# copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2026 by Garth Snyder. All rights reserved.
+#
+
 import argparse
 import subprocess
 import sys
