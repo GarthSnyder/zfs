175203d9718588c36bcff0a21f522962dea18faa
Garth Snyder <garth@garthsnyder.com>
2026-05-18 16:46:02 -0700

    Fix typo

--- cmd/zstream/zstream_io.h
+++ cmd/zstream/zstream_io.h
@@ -3,7 +3,7 @@
  * CDDL HEADER START
  *
  * This file and its contents are supplied under the terms of the Common
- * Development and Distribution License ("CDDL")k, version 1.0. You may only use
+ * Development and Distribution License ("CDDL"), version 1.0. You may only use
  * this file in accordance with the terms of version 1.0 of the CDDL.
  *
  * A full copy of the text of the CDDL should have accompanied this source. A
