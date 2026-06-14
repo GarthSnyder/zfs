2fa1eff527766719959664b2d0331e6759bbd26b
Garth Snyder <garth@garthsnyder.com>
2026-06-14 16:53:07 -0700

    Set logical size to former compressed size rather than payload size in zstream_decompress.c

--- cmd/zstream/zstream_decompress.c
+++ cmd/zstream/zstream_decompress.c
@@ -78,8 +78,8 @@ chain_decompress_named_writes(drr_packet_t *item, void *context)
 	}
 	if (ctype_is_uncompressed(ctype)) {
 		drrw->drr_compressiontype = 0;
+		drrw->drr_logical_size = drrw->drr_compressed_size;
 		drrw->drr_compressed_size = 0;
-		drrw->drr_logical_size = item->dp_payload_size;
 		if (OPTION_ENABLED(CA_VERBOSE)) {
 			fprintf(stderr,
 			    "Resetting compression type to "
